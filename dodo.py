8b584048caffbd524bec78607b8645d2e7380f67
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-01-31 11:29:26 -0600

    start postmortem for 0.1.2

--- dodo.py
+++ dodo.py
@@ -17,8 +17,8 @@ class C:
 
     NAME = "jyg"
     NPM_NAME = f"@deathbeds/{NAME}"
-    OLD_VERSION = "0.1.1"
-    VERSION = "0.1.2"
+    OLD_VERSION = "0.1.2"
+    VERSION = "0.1.3"
     PACKAGE_JSON = "package.json"
     PYPROJECT_TOML = "pyproject.toml"
     PLATFORM = platform.system()
